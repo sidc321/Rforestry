eb75a446625d30fca632a94b3b61bab82cbd48c0
Sam Antonyan <sam.ant.20031980@gmail.com>
2022-04-24 17:54:07 -0400

    Organizing and working on the preprocessing file

--- Python_package/Py_preprocessing.py
+++ Python_package/Py_preprocessing.py
@@ -1,6 +1,296 @@
-import pandas as pd
+"""
+LIBRARIES NEEDED
+----------------------
+"""
+
 import numpy as np
+import pandas as pd
 import warnings
+import math
+import os
+import sys
+from random import randrange
+
+
+
+"""
+--------------------------------------
+
+#' @include the preprocessign file
+#-- Sanity Checker -------------------------------------------------------------
+#' @name training_data_checker
+#' @title Training data check
+#' @rdname training_data_checker-forestry
+#' @description Check the input to forestry constructor
+#' @inheritParams forestry
+#' @param featureWeights weights used when subsampling features for nodes above or at interactionDepth.
+#' @param deepFeatureWeights weights used when subsampling features for nodes below interactionDepth.
+#' @param hasNas indicates if there is any missingness in x.
+#' @return A list of parameters after checking the selected parameters are valid.
+"""
+def training_data_checker(
+    x,
+    y,
+    ntree,
+    replace,
+    sampsize,
+    mtry,
+    nodesizeSpl,
+    nodesizeAvg,
+    nodesizeStrictSpl,
+    nodesizeStrictAvg,
+    minSplitGain,
+    maxDepth,
+    interactionDepth,
+    splitratio,
+    OOBhonest,
+    nthread,
+    middleSplit,
+    doubleTree,
+    linFeats,
+    monotonicConstraints,
+    groups,
+    featureWeights,
+    deepFeatureWeights,
+    observationWeights,
+    linear,
+    symmetric,
+    scale,
+    hasNas
+):
+    
+    x = pd.DataFrame(x)
+    y = np.array(y)
+    nrows, nfeatures = x.shape
+
+    # Check if the input dimension of x matches y
+    if nrows != len(y):
+        raise ValueError('The dimension of input dataset x doesn\'t match the output y.')
+
+    if linear and hasNas:
+        raise ValueError('Cannot do imputation splitting with linear.')
+
+    if np.isnan(y).any():
+        raise ValueError('y contains missing data.')
+
+    if not isinstance(replace, bool):
+        raise ValueError('replace must be True or False.')
+
+    if (not isinstance(ntree, int)) or ntree <= 0:
+        raise ValueError('ntree must be a positive integer.')
+
+    if (not isinstance(sampsize, int)) or sampsize <= 0:
+        raise ValueError('sampsize must be a positive integer.')
+
+    if any(i < 0 or i >= nfeatures for i in linFeats):
+        raise ValueError('linFeats must contain positive integers less than len(x.columns).')
+
+    if (not replace) and sampsize > nrows:
+        raise ValueError('You cannot sample without replacement with size more than total number of observations.')
+
+    if (not isinstance(mtry, int)) or mtry <= 0:
+        raise ValueError('mtry must be a positive integer.')
+
+    if mtry > nfeatures:
+        raise ValueError('mtry cannot exceed total amount of features in x.')
+
+    if (not isinstance(nodesizeSpl, int)) or nodesizeSpl <= 0:
+        raise ValueError('nodesizeSpl must be a positive integer.')
+
+    if (not isinstance(nodesizeAvg, int)) or nodesizeAvg <= 0:
+        raise ValueError('nodesizeAvg must be a positive integer.')
+
+    if (not isinstance(nodesizeStrictSpl, int)) or nodesizeStrictSpl <= 0:
+        raise ValueError('nodesizeStrictSpl must be a positive integer.')
+
+    if (not isinstance(nodesizeStrictAvg, int)) or nodesizeStrictAvg <= 0:
+        raise ValueError('nodesizeStrictAvg must be a positive integer.')
+
+    if minSplitGain < 0:
+        raise ValueError('minSplitGain must be greater than or equal to 0.')
+
+    if minSplitGain > 0 and (not linear):
+        raise ValueError('minSplitGain cannot be set without setting linear to be true.')
+
+    if (not isinstance(maxDepth, int)) or maxDepth <= 0:
+        raise ValueError('maxDepth must be a positive integer.')
+
+    if (not isinstance(interactionDepth, int)) or interactionDepth <= 0:
+        raise ValueError('interactionDepth must be a positive integer.')
+
+    if len(monotonicConstraints) != nfeatures:
+        raise ValueError('monotonicConstraints must be the size of x')
+    
+    if any(i != 0 and i != 1 and i != -1 for i in monotonicConstraints):
+        raise ValueError('monotonicConstraints must be either 1, 0, or -1')
+
+    if any(i != 0 for i in monotonicConstraints) and linear:
+        raise ValueError('Cannot use linear splitting with monotonicConstraints')
+
+    if not replace:
+        observationWeights = [1 for _ in range(nrows)]
+
+    if len(observationWeights) != nrows:
+        raise ValueError('observationWeights must have length len(x)')
+
+    if any(i < 0 for i in observationWeights):
+        raise ValueError('The entries in observationWeights must be non negative')
+
+    if sum(observationWeights) == 0:
+        raise ValueError('There must be at least one non-zero weight in observationWeights')
+
+    if any(i != 0 for i in symmetric):
+        if linear:
+           raise ValueError('Symmetric forests cannot be combined with linear aggregation please set either symmetric = False or linear = False') 
+
+        if hasNas:
+            raise ValueError('Symmetric forests cannot be combined with missing values please impute the missing features before training a forest with symmetry')
+
+        if scale:
+            warnings.warn('As symmetry is implementing pseudo outcomes, this causes problems when the Y values are scaled. Setting scale = False')
+
+        # for now don't scale when we run symmetric splitting since we use pseudo outcomes
+        # and wnat to retain the scaling of Y
+        scale = False
+
+        #OPTIMIZE ???
+        if any(j != 1 and j != 0 for j in symmetric):
+            raise ValueError('Entries of the symmetric argument must be zero one')
+
+        if sum(j > 0 for j in symmetric) > 10:
+            warnings.warn('Running symmetric splits in more than 10 features is very slow')
+
+    
+    s = sum(observationWeights)
+    observationWeights = [i/s for i in observationWeights]
+
+    # if the splitratio is 1, then we use adaptive rf and avgSampleSize is
+    # equal to the total sampsize
+
+    if splitratio == 0 or splitratio == 1:
+        splitSampleSize = sampsize
+        avgSampleSize = sampsize
+    else:
+        splitSampleSize = splitratio * sampsize
+        avgSampleSize = math.floor(sampsize - splitSampleSize)
+        splitSampleSize = math.floor(splitSampleSize)
+
+    if nodesizeStrictSpl > splitSampleSize:
+        warnings.warn('nodesizeStrictSpl cannot exceed splitting sample size. We have set nodesizeStrictSpl to be the maximum.')
+        nodesizeStrictSpl = splitSampleSize
+
+    if nodesizeStrictAvg > avgSampleSize:
+        warnings.warn('nodesizeStrictAvg cannot exceed averaging sample size. We have set nodesizeStrictAvg to be the maximum.')
+        nodesizeStrictAvg = avgSampleSize
+
+
+    if doubleTree:
+        if splitratio == 0 or splitratio == 1:
+            warnings.warn('Trees cannot be doubled if splitratio is 1. We have set doubleTree to False.')
+            doubleTree = False
+        else:
+            if nodesizeStrictAvg > splitSampleSize:
+                warnings.warn('nodesizeStrictAvg cannot exceed splitting sample size. We have set nodesizeStrictAvg to be the maximum.')
+                nodesizeStrictAvg = splitSampleSize
+            if nodesizeStrictSpl > avgSampleSize:
+                warnings.warn('nodesizeStrictSpl cannot exceed averaging sample size. We have set nodesizeStrictSpl to be the maximum.')
+                nodesizeStrictSpl = avgSampleSize
+
+    if splitratio < 0 or splitratio > 1:
+        raise ValueError('splitratio must in between 0 and 1.')
+
+    if groups is not None:
+        if not pd.api.types.is_categorical_dtype(groups):
+            raise ValueError('groups must have a data dtype of categorical. Try using pd.Series(..., dtype="category") or pd.Categorical(...).')
+        if len(groups.unique()) == 1:
+            raise ValueError('groups must have more than 1 level to be left out from sampling.')
+        groups = pd.Series(groups, dtype='category')
+
+    if OOBhonest and (splitratio != 1):
+        warnings.warn('OOBhonest is set to true, so we will run OOBhonesty rather than standard honesty.')
+        splitratio = 1
+
+    if OOBhonest and (replace == False):
+        warnings.warn('replace must be set to TRUE to use OOBhonesty, setting this to True now')
+        replace = True
+
+    if nthread > 0:
+        if nthread > os.cpu_count():
+            raise ValueError('nthread cannot exceed total cores in the computer: ' + str(os.cpu_count()))
+      
+    if not isinstance(middleSplit, bool):
+        raise ValueError('middleSplit must be True or False.')
+
+    return (
+        x,
+        y,
+        ntree,
+        replace,
+        sampsize,
+        mtry,
+        nodesizeSpl,
+        nodesizeAvg,
+        nodesizeStrictSpl,
+        nodesizeStrictAvg,
+        minSplitGain,
+        maxDepth,
+        interactionDepth,
+        splitratio,
+        OOBhonest,
+        nthread,
+        groups,
+        middleSplit,
+        doubleTree,
+        linFeats,
+        monotonicConstraints,
+        featureWeights,
+        scale,
+        deepFeatureWeights,
+        observationWeights,
+        hasNas
+    )
+
+
+"""
+@title Test data check
+@name testing_data_checker-forestry
+@description Check the testing data to do prediction
+@param object A forestry object.
+@param newdata A data frame of testing predictors.
+@param hasNas TRUE if the there were nan-s in the training data FALSE otherwise.
+@return A feature dataframe if it can be used for new predictions.
+"""
+def testing_data_checker(object, newdata, hasNas):
+    pass
+
+
+
+def sample_weights_checker(featureWeights, mtry, ncol):
+    if len(featureWeights) != ncol:
+        raise ValueError('featureWeights and deepFeatureWeights must have length len(x.columns)')
+
+    if any(i < 0 for i in featureWeights):
+        raise ValueError('The entries in featureWeights and deepFeatureWeights must be non negative')
+
+    if sum(featureWeights) == 0:
+        raise ValueError('There must be at least one non-zero weight in featureWeights and deepFeatureWeights')
+
+
+    featureWeightsVariables = [i for i in range(len(featureWeights)) if featureWeights[i] > max(featureWeights)*0.001]
+    if len(featureWeightsVariables) < mtry:
+        featureWeights = []
+    
+    return (featureWeightsVariables, featureWeights)
+
+"""
+Checks if forestry object has valid pointer for C++ object.
+@param object a forestry object
+@return A message if the forest does not have a valid C++ pointer.
+"""
+def forest_checker(object):
+    pass
+
+
 
 
 
@@ -28,3 +318,45 @@ def preprocess_training(x,y):
 
     #################################
     return (x, [], [])
+
+
+#' @title preprocess_testing
+#' @description Perform preprocessing for the testing data, including converting
+#'   data to dataframe, and testing if the columns are consistent with the
+#'   training data and encoding categorical data into numerical representation
+#'   in the same way as training data.
+#' @inheritParams forestry
+#' @param categoricalFeatureCols A list of index for all categorical data. Used
+#'   for trees to detect categorical columns.
+#' @param categoricalFeatureMapping A list of encoding details for each
+#'   categorical column, including all unique factor values and their
+#'   corresponding numeric representation.
+#' @return A preprocessed training dataaset x
+def preprocess_testing (x, categoricalFeatureCols, categoricalFeatureMapping):
+    pass
+
+
+
+#' @title scale_center
+#' @description Given a dataframe, scale and center the continous features
+#' @param x A dataframe in order to be processed.
+#' @param categoricalFeatureCols A vector of the categorical features, we
+#'   don't want to scale/center these. Should be 1-indexed.
+#' @param colMeans A vector of the means to center each column.
+#' @param colSd A vector of the standard deviations to scale each column with.
+#' @return A scaled and centered  dataset x
+def scale_center(x, categoricalFeatureCols, colMeans, colSd):
+    pass
+
+
+
+#' @title unscale_uncenter
+#' @description Given a dataframe, un scale and un center the continous features
+#' @param x A dataframe in order to be processed.
+#' @param categoricalFeatureCols A vector of the categorical features, we
+#'   don't want to scale/center these. Should be 1-indexed.
+#' @param colMeans A vector of the means to add to each column.
+#' @param colSd A vector of the standard deviations to rescale each column with.
+#' @return A dataset x in it's original scaling
+def unscale_uncenter(x, categoricalFeatureCols, colMeans, colSd):
+    pass
---
+++ Python_package/test.py
@@ -0,0 +1,51 @@
+from cmath import nan
+from tkinter.messagebox import RETRY
+from unicodedata import category
+import numpy as np
+import pandas as pd
+import warnings
+import math
+import os
+from random import randrange
+import sys
+from forestry import forestry
+import Py_preprocessing
+from sklearn.ensemble import RandomForestClassifier
+
+df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                   columns=['a', 'b', 'c'])
+
+y = [0, 0, 1]
+res = Py_preprocessing.training_data_checker(
+    x = df2,
+    y = y,
+    ntree = 3,
+    replace = True,
+    sampsize = 4,
+    mtry = 3,
+    nodesizeSpl = 2,
+    nodesizeAvg = 1,
+    nodesizeStrictSpl = 2,
+    nodesizeStrictAvg = 1,
+    minSplitGain = 0,
+    maxDepth = 10,
+    interactionDepth = 8,
+    splitratio = 0.5,
+    OOBhonest = 0,
+    nthread = 7,
+    middleSplit = True,
+    doubleTree = 0,
+    linFeats = [0, 1, 2],
+    monotonicConstraints = [0, 0, 0],
+    groups = pd.Series([1,2,3], dtype="category"),
+    featureWeights = 0,
+    deepFeatureWeights = 0,
+    observationWeights = [1, 2, 3],
+    linear = False,
+    symmetric = [0,1,0,1],
+    scale = False,
+    hasNas = False
+)
+
+
+PF1 = forestry(x=df2, y=y, groups=pd.Categorical([1,1,1,2,3,2,3,2,3,3,4,2,1,2,2,3,3,3,4]), minTreesPerGroup=100)
