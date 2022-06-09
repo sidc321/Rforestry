0231a8734bd1c6736f2c115aaca5b2dbfcbf0e15
Theo S <theo_s@berkeley.edu>
2022-06-09 09:46:37 -0700

    * Add more plotting examples to test.py

--- python-package/test.py
+++ python-package/test.py
@@ -20,19 +20,31 @@ X = X.loc[:, X.columns != 'sepal length (cm)']
 y = df['sepal length (cm)']
 
 fr = forestry(
-        ntree = 1,
+        ntree = 500,
         maxDepth=2,
-        verbose=True,
+        verbose=False,
         scale=False
 )
 
 print("Fitting the forest")
 fr.fit(X, y)
 
-#print("Predicting with the forest")
-#forest_preds = fr.predict(aggregation='oob')
-#print(forest_preds)
+#%%
 
+print("Predicting with the forest")
+forest_preds = fr.predict(aggregation='oob')
+print(forest_preds)
+
+#%%
+
+f
+
+#%%
+print("Get the variable importance")
+print(fr.getVI())
+
+print("Get the OOB Error")
+print(fr.getOOB())
 
 #%%
 # Now try getting a tree translated
@@ -44,20 +56,18 @@ from sklearn.datasets import *
 from sklearn import tree
 from dtreeviz.trees import *
 import platform
-#from dtreeviz.models.sklearn_decision_trees import ShadowSKDTree
+from dtreeviz.models.sklearn_decision_trees import ShadowSKDTree
 from forestry_shadow import ShadowForestryTree
 
 #%%
 
-regr = tree.DecisionTreeRegressor(max_depth=2)
+regr = tree.DecisionTreeRegressor(max_depth=3)
 boston = load_boston()
 regr.fit(boston.data, boston.target)
 
-print(regr.decision_path(boston.data))
-
-path_data = regr.decision_path(boston.data)
-
-shadow_dtree = ShadowForestryTree(fr, boston.data, boston.target, boston.feature_names, "price")
+#print(regr.decision_path(boston.data))
+#path_data = regr.decision_path(boston.data)
+shadow_dtree = ShadowSKDTree(regr, boston.data, boston.target, boston.feature_names, "price")
 
 
 #%%
@@ -67,8 +77,47 @@ viz = dtreeviz(shadow_dtree,
                boston.target,
                target_name='price',
                feature_names=boston.feature_names,
-               scale=3.0)
+               scale=2.0)
 viz.view()
 
 
 #%%
+# See a classification example
+clas = tree.DecisionTreeClassifier(max_depth=2)
+iris = load_iris()
+
+X_train = iris.data
+y_train = iris.target
+clas.fit(X_train, y_train)
+
+viz = dtreeviz(clas,
+               X_train,
+               y_train,
+               target_name='Species',
+               scale = 3.0,
+               feature_names=iris.feature_names,
+               class_names=["setosa", "versicolor", "virginica"],
+               histtype= 'barstacked')  # barstackes is default
+viz.view()
+
+#%%
+
+clf = tree.DecisionTreeClassifier(max_depth=2)
+wine = load_wine()
+
+X_train = wine.data
+y_train = wine.target
+clf.fit(X_train, y_train)
+
+# pick random X observation for demo
+X = wine.data[np.random.randint(0, len(wine.data)),:]
+
+viz = dtreeviz(clf,
+               wine.data,
+               wine.target,
+               target_name='wine',
+               scale = 2.0,
+               feature_names=wine.feature_names,
+               class_names=list(wine.target_names),
+               X=X)  # pass the test observation
+viz.view()
