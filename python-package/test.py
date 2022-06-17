65fc9206dea1eeedc6dd654513e6247bcfa02c2d
Theo S <theo_s@berkeley.edu>
2022-06-17 16:25:58 -0700

    * Update the test.py file

--- python-package/test.py
+++ python-package/test.py
@@ -8,17 +8,35 @@ from random import randrange
 import sys
 from forestry import forestry
 import Py_preprocessing
-from sklearn.datasets import load_iris
 
+from sklearn.datasets import *
+from sklearn import tree
+from dtreeviz.trees import *
+import platform
+from dtreeviz.models.sklearn_decision_trees import ShadowSKDTree
+from forestry_shadow import ShadowForestryTree
+
+from sklearn.ensemble import RandomForestRegressor
 #%%
 
 # Load in the training data
+
 data = load_iris()
 df = pd.DataFrame(data['data'], columns=data['feature_names'])
 df['target'] = data['target']
 X = df.loc[:, df.columns != 'target']
 X = X.loc[:, X.columns != 'sepal length (cm)']
 y = df['sepal length (cm)']
+
+
+fr = forestry(
+        ntree = 500,
+        maxDepth=2,
+        verbose=False,
+        scale=False,
+        seed=1729
+)
+
 
 
 #%%
@@ -31,22 +49,23 @@ fr = forestry(
     #ntree = 1,
     maxDepth=3,
     seed=1,
-    verbose=False,
+    verbose=True,
     scale=False
 )
 
-#%%
-print("Fitting the forest")
-fr.fit(X, y)
 
+# print(fr.correctedPredict(X, nrounds=4, params_forestry={'ntree': 500, 'maxDepth': 2, 'verbose': False, 'scale': False}, feats=[0,1,2], simple=True, verbose=True, linear=True, keep_fits=True))
+# print(fr.get_params())
 #%%
 print("Predicting with the forest")
 forest_preds = fr.predict(newdata = X)
 print(forest_preds)
 #%%
 print(forest_preds)
+# shadow_forestry = ShadowForestryTree(fr, X, y, X.columns.values, 'sepal length (cm)', 1)
+# # X.columns.values
+# # #%%
 
-print(fr.getVI())
 
 #%%
 from sklearn.datasets import *
@@ -55,14 +74,39 @@ from dtreeviz.trees import *
 import platform
 
 
-#%%
+# # print(shadow_forestry.get_children_right())
+# # print(X.columns.values)
+# viz = dtreeviz(shadow_forestry,
+#                scale=3.0,
+#                target_name='sepal length (cm)',
+#                feature_names=X.columns.values)
 
-regr = tree.DecisionTreeRegressor(max_depth=2)
-boston = load_boston()
-regr.fit(boston.data, boston.target)
-viz = dtreeviz(regr,
-               boston.data,
-               boston.target,
-               target_name='price',
-               feature_names=boston.feature_names)
-viz.view()
+# viz.view()
+# #%%
+# # print(shadow_forestry.get_node_samples())
+# # print(shadow_forestry.get_split_samples(3)) --- Still wrong
+# # print(shadow_forestry.get_node_nsamples(6))
+
+# # print(shadow_forestry.get_score())
+
+
+# # Sklearn
+# #%%
+# regr = tree.DecisionTreeRegressor(max_depth=2)
+# regr.fit(X, y)
+
+# shadow_dtree = ShadowSKDTree(regr, X, y, X.columns.values, 'sepal length (cm)')
+
+# #%%
+# # print(shadow_dtree.get_children_left())
+# # print(shadow_dtree.get_children_right())
+# # print(shadow_dtree.get_features())
+# # dec_paths = regr.decision_path(X)
+# # print(dec_paths)
+
+
+sk_fr = RandomForestRegressor(n_estimators=500, max_depth=10, random_state=1729)
+sk_fr.fit(X, y)
+
+sk_fr.set_params(**{'n_estimators': 300, 'random_state': 1729})
+print(sk_fr.get_params())
