75c57a74f5d31cbcf2800f15467bc5dc88b798ab
Theo S <theo_s@berkeley.edu>
2022-07-11 18:18:05 -0700

    * Test for trainingIdx + weightMatrix

--- tests/testthat/test-trainingIdx.R
+++ tests/testthat/test-trainingIdx.R
@@ -10,13 +10,29 @@ test_that("Tests using trainingIdx when doing OOB predictions on smaller data", 
     # Test OOB Aggregation
     p_oob <- predict(rf, newdata = x_new, aggregation = "oob")
     p_oob_idx <- predict(rf, newdata = x_new[1:10,], aggregation = "oob", trainingIdx = 1:10)
-    expect_equal(all.equal(p_oob[1:10], p_oob_idx[1:10]), TRUE)
+    expect_equal(all.equal(p_oob[1:10], p_oob_idx), TRUE)
+
+    # Check weight matrices are equal
+    p_oob_weights <- predict(rf, newdata = x_new, aggregation = "oob", weightMatrix = TRUE)
+    p_oob_idx_weights <- predict(rf, newdata = x_new[1:10,], aggregation = "oob", trainingIdx = 1:10, weightMatrix = TRUE)
+
+    expect_equal(all.equal((p_oob_weights$weightMatrix %*% as.matrix(rf@processed_dta$y))[,1][1:10],
+                           (p_oob_idx_weights$weightMatrix %*% as.matrix(rf@processed_dta$y))[,1])
+                 , TRUE)
 
     # Test doubleOOB aggregation
     if (rf@doubleBootstrap) {
       p_doob <- predict(rf, newdata = x_new, aggregation = "doubleOOB")
       p_doob_idx <- predict(rf, newdata = x_new[1:10,], aggregation = "doubleOOB", trainingIdx = 1:10)
       expect_equal(all.equal(p_doob[1:10], p_doob_idx[1:10]), TRUE)
+
+      # Check weight matrix equality
+      p_doob_weights <- predict(rf, newdata = x_new, aggregation = "doubleOOB", weightMatrix = TRUE)
+      p_doob_idx_weights <- predict(rf, newdata = x_new[1:10,], aggregation = "doubleOOB", trainingIdx = 1:10, weightMatrix = TRUE)
+
+      expect_equal(all.equal((p_doob_weights$weightMatrix %*% as.matrix(rf@processed_dta$y))[,1][1:10],
+                             (p_doob_idx_weights$weightMatrix %*% as.matrix(rf@processed_dta$y))[,1])
+                   , TRUE)
     }
   }
 
@@ -62,17 +78,17 @@ test_that("Tests using trainingIdx when doing OOB predictions on smaller data", 
 
   # Check error handling =======================================================
   expect_error(
-    predict_OOBpreds <- predict(forest, aggregation = "average"),
+    predict_OOBpreds <- predict(forest_groups, aggregation = "average"),
     "When using an aggregation that is not oob or doubleOOB, one must supply newdata"
   )
 
   expect_error(
-    predict_OOBpreds <- predict(rf, aggregation = "oob", newdata = xtrain[1:30,]),
+    predict_OOBpreds <- predict(forest_groups, aggregation = "oob", newdata = xtrain[1:30,]),
     "trainingIdx must be set when doing out of bag predictions with a data set not equal in size to the training data set"
   )
 
   expect_error(
-    predict_OOBpreds <- predict(rf, aggregation = "oob", newdata = xtrain[1:30,], trainingIdx = 1:20),
+    predict_OOBpreds <- predict(forest_groups, aggregation = "oob", newdata = xtrain[1:30,], trainingIdx = 1:20),
     "The length of trainingIdx must be the same as the number of observations in the training data"
   )
 
