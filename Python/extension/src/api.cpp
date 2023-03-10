2e466f9c3c8c6527b6534fff8fa394cc3815aa54
Boban Petrovic <petrovicboban.g@googlemail.com>
2023-03-10 16:46:26 +0000

    Include utils.cpp to CMakeList

--- Python/extension/src/api.cpp
+++ Python/extension/src/api.cpp
@@ -9,26 +9,26 @@
 #include "forestryTree.h"
 
 
-extern "C"{
+extern "C" {
     void* get_data (
-            double* arr,
-            size_t* categorical_vars,
-            size_t countCategoricals,
-            size_t* linFeat_idx,
-            size_t countLinFeats,
-            double* feat_weights,
-            size_t* feat_weight_vars,
-            size_t countFtWeightVars,
-            double* deep_feat_weights,
-            size_t* deep_feat_weight_vars,
-            size_t countDeepFtWeightVars,
-            double* observation_weights,
-            int* mon_constraints,
-            size_t* groupMemberships,
-            bool monotoneAvg,
-            size_t numRows,
-            size_t numColumns,
-            unsigned int seed
+        double* arr,
+        size_t* categorical_vars,
+        size_t countCategoricals,
+        size_t* linFeat_idx,
+        size_t countLinFeats,
+        double* feat_weights,
+        size_t* feat_weight_vars,
+        size_t countFtWeightVars,
+        double* deep_feat_weights,
+        size_t* deep_feat_weight_vars,
+        size_t countDeepFtWeightVars,
+        double* observation_weights,
+        int* mon_constraints,
+        size_t* groupMemberships,
+        bool monotoneAvg,
+        size_t numRows,
+        size_t numColumns,
+        unsigned int seed
     ) {
         // Create Data: first n_cols - 1 are features, last is outcome
         std::unique_ptr< std::vector< std::vector<double> > > featureData {
@@ -166,33 +166,33 @@ extern "C"{
     
     
     void* train_forest(
-            void* data_ptr,
-            size_t ntree,
-            bool replace,
-            size_t sampSize,
-            double splitRatio,
-            bool OOBhonest,
-            bool doubleBootstrap,
-            size_t mtry,
-            size_t minNodeSizeSpt,
-            size_t minNodeSizeAvg,
-            size_t minNodeSizeToSplitSpt,
-            size_t minNodeSizeToSplitAvg,
-            double minSplitGain,
-            size_t maxDepth,
-            size_t interactionDepth,
-            unsigned int seed,
-            size_t nthread,
-            bool verbose,
-            bool splitMiddle,
-            size_t maxObs,
-            size_t minTreesPerFold,
-            size_t foldSize,
-            bool hasNas,
-            bool naDirection,
-            bool linear,
-            double overfitPenalty,
-            bool doubleTree
+        void* data_ptr,
+        size_t ntree,
+        bool replace,
+        size_t sampSize,
+        double splitRatio,
+        bool OOBhonest,
+        bool doubleBootstrap,
+        size_t mtry,
+        size_t minNodeSizeSpt,
+        size_t minNodeSizeAvg,
+        size_t minNodeSizeToSplitSpt,
+        size_t minNodeSizeToSplitAvg,
+        double minSplitGain,
+        size_t maxDepth,
+        size_t interactionDepth,
+        unsigned int seed,
+        size_t nthread,
+        bool verbose,
+        bool splitMiddle,
+        size_t maxObs,
+        size_t minTreesPerFold,
+        size_t foldSize,
+        bool hasNas,
+        bool naDirection,
+        bool linear,
+        double overfitPenalty,
+        bool doubleTree
     ) {
         DataFrame* test_df = reinterpret_cast<DataFrame* >(data_ptr);
         forestry* forest ( new (std::nothrow) forestry(
@@ -356,16 +356,16 @@ extern "C"{
     
     
     void predictOOB_forest(
-            void* forest_pt,
-            void* dataframe_pt,
-            double* test_data,
-            bool doubleOOB,
-            bool exact,
-            bool returnWeightMatrix,
-            bool verbose,
-            std::vector<double>& predictions,
-            std::vector<double>& weight_matrix,
-            std::vector<size_t> training_idx
+        void* forest_pt,
+        void* dataframe_pt,
+        double* test_data,
+        bool doubleOOB,
+        bool exact,
+        bool returnWeightMatrix,
+        bool verbose,
+        std::vector<double>& predictions,
+        std::vector<double>& weight_matrix,
+        std::vector<size_t> training_idx
     ) {
         if (verbose)
             std::cout << forest_pt << std::endl;
