f1e1a24169e6051683bbb9c244db0082583baa14
edwardwliu <edwardliu.w@gmail.com>
2023-04-21 11:41:42 -0400

    update group tests

--- Python/tests/test_groups.py
+++ Python/tests/test_groups.py
@@ -1,3 +1,4 @@
+import numpy as np
 from helpers import get_data
 from pandas import Series
 from random_forestry import RandomForest
@@ -6,18 +7,23 @@ from random_forestry import RandomForest
 def test_groups():
     X, y = get_data()
 
-    forest = RandomForest()
-    groups = Series(range(len(X)), dtype="category")
-    forest.fit(X, y, groups = groups)
+    forest = RandomForest(ntree=1)
+    forest.fit(X, y)
     pred = forest.predict(X, aggregation="average")
-    assert len(pred) == len(X)
+
+    forest = RandomForest(ntree=1)
+    groups = Series([i for i in range(1, len(X) // 2 + 1) for _ in range(2)])
+    forest.fit(X, y, groups = groups)
+    pred_groups = forest.predict(X, aggregation="average")
+
+    assert np.array_equal(pred, pred_groups)
 
 
 def test_groups_oob():
     X, y = get_data()
 
-    forest = RandomForest(oob_honest=True)
-    groups = Series([i for i in range(len(X) // 2) for _ in range(2)], dtype="category")
+    forest = RandomForest(ntree=1, oob_honest=True)
+    groups = Series([i for i in range(1, len(X) // 2 + 1) for _ in range(2)])
     forest.fit(X, y, groups = groups)
     pred = forest.predict(X, aggregation="oob", return_weight_matrix = True)
     assert len(pred["predictions"]) == len(X)
