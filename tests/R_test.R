55f8226ee617dd1ec59a3f5a756d402e4d489495
edwardwliu <edwardliu.w@gmail.com>
2023-04-22 12:26:38 -0400

    add cross language tests for default params and honesty

--- tests/R_test.R
+++ tests/R_test.R
@@ -1,2 +1,30 @@
-library(Rforestry);
-write('hello world!', stdout())
+suppressMessages(library(Rforestry))
+
+stdout <- stdout()
+test_stdout <- function(testName, results) {
+    write(paste0("=== ", testName, " ==="), stdout)
+    results <- round(results, 6)
+    for (res in results) {
+        write(sprintf("%.6f", res), stdout)
+    }
+}
+
+x <- iris [, -c(1, 5)]
+y <- iris[, 1]
+
+# Test default parameters
+forest <- forestry(x, y, scale = FALSE, seed = 2)
+pred_avg <- predict(forest, x, aggregation = "average")
+pred_oob <- predict(forest, x, aggregation = "oob")
+
+test_stdout("Default parameters: average", pred_avg)
+test_stdout("Default parameters: oob", pred_oob)
+
+# Test oob honest
+forest <- forestry(x, y, scale = FALSE, seed = 2, OOBhonest = TRUE)
+pred_avg <- predict(forest, x, aggregation = "average")
+pred_oob <- predict(forest, x, aggregation = "oob")
+
+test_stdout("OOB honest: average", pred_avg)
+test_stdout("OOB honest: oob", pred_oob)
+
--- tests/python_test.py
+++ tests/python_test.py
@@ -1,3 +1,48 @@
-import random_forestry
+import os
+import sys
+from contextlib import contextmanager
 
-print("hello world!")
+from numpy import ndarray
+from random_forestry import RandomForest
+from sklearn import datasets
+
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout, sys.stdout = sys.stdout, devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+
+def test_stdout(test_name: str, results: ndarray) -> None:
+    print("=== " + test_name + " ===")
+    for res in results:
+        print(f"{res:.6f}")
+
+
+iris = datasets.load_iris()
+X = iris.data[:, 1:4]
+y = iris.data[:, 0]
+
+# Test default parameters
+with suppress_stdout():
+    forest = RandomForest(seed=2)
+    forest.fit(X, y)
+    pred_avg = forest.predict(X, aggregation="average")
+    pred_oob = forest.predict(X, aggregation="oob")
+
+test_stdout("Default parameters: average", pred_avg)
+test_stdout("Default parameters: oob", pred_oob)
+
+# Test oob honest
+with suppress_stdout():
+    forest = RandomForest(seed=2, oob_honest=True)
+    forest.fit(X, y)
+    pred_avg = forest.predict(X, aggregation="average")
+    pred_oob = forest.predict(X, aggregation="oob")
+
+test_stdout("OOB honest: average", pred_avg)
+test_stdout("OOB honest: oob", pred_oob)
